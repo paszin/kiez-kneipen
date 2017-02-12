c1ea455e13565e9c7916e64d41b49bc4055a2452
paszin <pascalslogin@gmail.com>
2017-02-12 02:53:23 +0100

    seperated locations

---
+++ locations.js
@@ -0,0 +1,48 @@
+var locations = [{
+        coordinates: [52.486298, 13.363686],
+        name: "Jonas Kneipe",
+        type: "kneipe",
+        hasPool: true,
+        hasDarts: true,
+        hasKicker: false,
+        hasBuli: false,
+        getIsOpen: function() {return true;}
+
+    },
+    {
+        coordinates: [52.488265, 13.369281],
+        name: "Blues Garage",
+        type: "kneipe",
+        hasPool: true,
+        hasDarts: null,
+        hasKicker: true,
+        hasBuli: true
+    },
+    {
+        coordinates: [52.480542, 13.350699],
+        name: "Gemütlichkeit bei Rena",
+        type: "kneipe"
+    },
+    {
+        coordinates: [52.482117, 13.351443],
+        name: "Otto Schruppke",
+        url: "",
+        type: "kneipe"
+    },
+    {
+        coordinates: [52.479248, 13.349706],
+        name: "Resonanz",
+        url: "https://www.google.de/maps/place/Resonanz",
+        type: "kneipe"
+    },
+    {
+        coordinates: [52.487435, 13.375139],
+        name: "Pavlov's",
+        url: "https://www.google.de/maps/place/Pavlov%27s",
+        type: "kneipe"
+    },{
+    coordinates: [52.480824, 13.349919],
+    name: "Café de Enrico",
+    type: "cafe"
+  }
+];
